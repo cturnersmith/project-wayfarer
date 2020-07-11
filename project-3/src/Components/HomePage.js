485d1e1b998b4fec26abbf9113710226475ec761
arblunt <theamberrenee@gmail.com>
2020-07-11 12:45:07 -0400

    Homepage Setup

--- project-3/src/Components/HomePage.js
+++ project-3/src/Components/HomePage.js
@@ -1,15 +1,36 @@
 import React from 'react';
 
-function HomePage(props) {
+function Homepage(props) {
     return(
 
-        <div className = "HomePage">
+        <div className = "Homepage">
             <h1>Welcome to Wayfarar!</h1>
-            <p></p>
+            <h2>Wayfarar: Ladies Night out</h2>
+            <ul>    
+                
+                <p><h3>Topic</h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                    pariatur.</p>
+                     
+                <p><h3>Topic</h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                    </p>
+                     
+                <p><h3>Topic</h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                    </p>
+            </ul>
             
         </div>
 
     )
 }
 
-export default HomePage;
+export default Homepage;
